459b8167272094678630bc3c508f57c13e4a965a
Avnish Ranjan <avnishranjan7@gmail.com>
2026-03-13 16:55:53 +0530

    operatore

---
+++ 03.Operatore.py
@@ -0,0 +1,27 @@
+#Arithemetic operatore
+a = 7
+b = 4
+c= a+b
+print(c)
+
+#Assignment Operaatore
+
+a= 4-2
+print(a)
+
+b= 6
+b +=3 #incremet the value of by 3 and then  assign it to B
+print(b)
+
+#Comparison operatore
+A= 5>1
+print(A)
+
+a= 5<=5
+print(a)
+
+#Logical operatotre
+
+if 5>2:
+    print("YES")
+    
